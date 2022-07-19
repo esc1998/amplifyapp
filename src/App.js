0597be5ebb689f88bd2bb656c388ae8c47b7aedc
esc1998 <lge.stella.cho@gmail.com>
2022-07-19 16:07:06 +0900

    message

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-        <h1> Hello from AI/Big Data Project </h1>
+        <h1> Hello from AI/Big Data Project v2 </h1>
       </header>      
     </div>
   );
